b50918edc42cbfd408842b8fd41dcc37d243f7e3
Bartek <bartlomiej.kacki@gmail.com>
2024-11-11 18:09:31 +0100

    HexDump uses command line now

--- HexDump/Program.cs
+++ HexDump/Program.cs
@@ -1,33 +1,39 @@
-﻿namespace HexDump
+﻿using System.ComponentModel.Design;
+using System.Text;
+
+namespace HexDump
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             var position = 0;
-            string fileName = "data.txt";
+            string fileName = "binarydata.dat"; //binarydata.dat data.txt
 
-            using (var reader = new StreamReader(fileName))
+            using (Stream input = File.OpenRead(args[0]))
             {
-                while (!reader.EndOfStream)
+                var buffer = new byte[16];
+                int bytesRead;
+
+                while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0)
                 {
-                    var buffer = new char[16];
-
-                    var bytesRead = reader.ReadBlock(buffer, 0, 16);
-
                     Console.Write("{0:x4}: ", position);
                     position += bytesRead;
 
                     for (var i = 0; i < 16; i++)
                     {
                         if (i < bytesRead)
+                        {
+                            if (buffer[i] < 0x20 || buffer[i] > 0x7F) buffer[i] = (byte)'.';
+
                             Console.Write("{0:x2} ", (byte)buffer[i]);
+                        }
                         else
                             Console.Write("   ");
                         if (i == 7) Console.Write("-- ");
                     }
 
-                    var bufferContents = new string(buffer);
+                    var bufferContents = Encoding.UTF8.GetString(buffer);
                     Console.WriteLine("\t{0}", bufferContents.Substring(0, bytesRead));
                 }
             }
